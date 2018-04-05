7e0b1f4ffbc5c7fc1c25eae036734d329673683e
heitorbelem19 <32557513+heitorbelem19@users.noreply.github.com>
2018-04-05 00:37:32 -0300

    Create dominios.h

---
+++ dominios.h
@@ -0,0 +1,71 @@
+#ifndef _DOMINIO_H_INCLUDED
+#define _DOMINIO_H_INCLUDED
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Nome{
+private:
+    string nome;
+    void validar(string nome)throw (invalid_argument);
+public:
+    void setNome(string nome)throw (invalid_argument);
+    string getNome();
+};
+
+class Sobrenome{
+private:
+	string sobrenome;
+	void validar(string sobrenome)throw (invalid_argument);
+public:
+	void setSobrenome(string sobrenome)throw (invalid_argument);
+	string getSobrenome();
+};
+
+class Telefone{
+private:
+	string telefone;
+	void validar(string telefone)throw (invalid_argument);
+public:
+	void setTelefone(string telefone)throw (invalid_argument);
+	string getTelefone();
+};
+
+class Endereco{
+private:
+	string endereco;
+	void validar(string endereco)throw (invalid_argument);
+public:
+	void setEndereco(string endereco)throw (invalid_argument);
+	string getEndereco();
+};
+
+class Data{
+private:
+	string data;
+	void validar(string data)throw (invalid_argument);
+public:
+	void setData(string data)throw (invalid_argument);
+	string getData();
+};
+
+class Texto{
+private:
+	string texto;
+	void validar(string texto)throw (invalid_argument);
+public:
+	void setTexto(string texto)throw (invalid_argument);
+	string getTexto();
+};
+
+class Idioma{
+private:
+	string idioma;
+	void validar()throw (invalid_argument);
+public:
+	void setIdioma(string idioma)throw (invalid_argument);
+	string getIdioma();
+};
+
+
+
