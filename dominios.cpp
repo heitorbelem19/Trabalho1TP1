9b1b0909811af666112e0df2d82c6429ba46f11b
heitorbelem19 <32557513+heitorbelem19@users.noreply.github.com>
2018-04-06 17:49:39 -0300

    Update dominios.cpp

--- dominios.cpp
+++ dominios.cpp
@@ -2,22 +2,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 //Métodos da Classe Nome
-void Nome::setNome(string nome)throw (invalid_argument){
-    validar(nome);
-    this->nome = nome;
-}
-
-string Nome::getNome(){
-    return this->nome;
-}
 
 void Nome::validar(string nome)throw (invalid_argument){
     int indice = 1;
     if(islower(nome[0]))
         throw invalid_argument ("Primeira letra deve ser Maiuscula!\n");
-    if(nome.length() >= 20)
+    if(nome.length() > 20)
         throw invalid_argument ("Nome ultrapassa limite permitido\n");
 
     while(nome[indice] != '\0'){
@@ -27,25 +18,17 @@ void Nome::validar(string nome)throw (invalid_argument){
             throw invalid_argument ("Nao pode possuir mais de uma letra maiuscula ou caracteres especiais!\n");
     }
 }
+
+
 //--------------------------------------------------------------------
 
-
 //Métodos da Classe Sobrenome
-void Sobrenome::setSobrenome(string Sobrenome)throw (invalid_argument){
-    validar(Sobrenome);
-    this->sobrenome = sobrenome;
-}
-
-string Sobrenome::getSobrenome(){
-    return this->sobrenome;
-}
-
 void Sobrenome::validar(string sobrenome)throw (invalid_argument){
     int indice = 1;
     if(islower(sobrenome[0]))
         throw invalid_argument ("Primeira letra deve ser Maiuscula!\n");
 
-    if(sobrenome.length() >= 20)
+    if(sobrenome.length() > 20)
         throw invalid_argument ("Sobrenome ultrapassa limite permitido\n");
 
     while(sobrenome[indice] != '\0'){
@@ -58,17 +41,7 @@ void Sobrenome::validar(string sobrenome)throw (invalid_argument){
 //------------------------------------------------------------------------
 
 
-
-
 //Métodos da Classe Telefone
-void Telefone::setTelefone(string telefone)throw (invalid_argument){
-    this->telefone = telefone;
-}
-
-string Telefone::getTelefone(){
-    return this->telefone;
-}
-
 void Telefone::validar(string telefone)throw (invalid_argument){
     for(int indice = 0; indice < telefone.length() - 1; indice++){
         if(indice == 0 || indice == 1){
@@ -76,14 +49,19 @@ void Telefone::validar(string telefone)throw (invalid_argument){
                 throw invalid_argument ("DDD nao pode conter caracteres que nao sejam numeros\n");
             }
         }
-        else if (indice == 8){
+         else if (indice == 2){
+            if(!isspace(telefone[indice])){
+                throw invalid_argument ("Numero de telefone invalido 1\n");
+            }
+        }
+        else if (indice == 7){
             if(telefone[indice] != '-'){
-                throw invalid_argument ("Numero de telefone invalido\n");
+                throw invalid_argument ("Numero de telefone invalido 1\n");
             }
         }
         else{
             if(!isdigit(telefone[indice])){
-                throw invalid_argument ("Numero de telefone invalido\n");
+                throw invalid_argument ("Numero de telefone invalido 1\n");
             }
         }
     }
@@ -92,107 +70,161 @@ void Telefone::validar(string telefone)throw (invalid_argument){
 
 
 //Métodos da Classe Endereço
-void Endereco::setEndereco(string endereco)throw (invalid_argument){
-    this->endereco = endereco;
-}
-
-string Endereco::getEndereco(){
-    return this->endereco;
-}
-
 void Endereco::validar(string endereco)throw (invalid_argument){
-    if(endereco.length() >= 20)
+    if(endereco.length() > 20)
         throw invalid_argument ("Endereco ultrapassa limite permitido\n");
-    if(isspace(endereco[0]) || isspace(endereco[endereco.length()]-1))
+    if(isspace(endereco[0]) || isspace(endereco[endereco.length()-1]))
         throw invalid_argument ("Nao pode haver espacos em branco no comeco ou no fim do campo Endereco\n");
     for(int indice = 0; indice < endereco.length(); indice++){
-        if(endereco[indice] == ' ' && endereco[indice+1] == ' ')
+        if(isspace(endereco[indice]) && isspace(endereco[indice + 1]))
             throw invalid_argument ("Nao pode haver espacos em branco seguidos\n");
     }
 }
 //------------------------------------------------------------------------
 
-
 //Métodos da Classe Data
-void Data::setData(string data)throw (invalid_argument){
-    this->data = data;
-}
-
-string Data::getData(){
-    return this->data;
-}
-
-void Data::validar(string data)throw (invalid_argument){
-
-}
-
-
-// Metodos de classe para o correio eletronico
-void Email::setEmail(string email)throw (invalid_argument){
-    this->email = email;
-}
-
-string Email:getEmail(){
-    return this->email
-}
-
-void Email::validar(string email)throw (invalid_argument){
-    for(indice == 0; indice < email.length();indice++){
-        if(email[0] || email)
+void Data::validar(string data) throw(invalid_argument){
+    int dia, mes, ano;
+    bool bissexto;
+    for(int indice = 0; indice < data.length()-1;indice++){
+        if(indice == 2 || indice == 5){
+            if(data[indice] != '/'){
+                throw invalid_argument("Formato errado de data\n");
+            }
+        }
+        else{
+            if(!isdigit(data[indice])){
+                throw invalid_argument("Formato invalido para os D,M,A\n");
+            }
+        }
     }
+    if ( (data[0] >= '0' && data[0] <= '9') && (data[1] >= '0' && data[1] <= '9') ){
+        dia  = ((data[0] - '0')* 10) + ((data[1] - '0'));
+    }
+    if ( (data[3] >= '0' && data[3] <= '9') && (data[4] >= '0' && data[4] <= '9') ){
+        mes  = ((data[3] - '0')* 10) + ((data[4] - '0'));
+    }
+    if ( (data[6] >= '0' && data[6] <= '9') && (data[7] >= '0' && data[7] <= '9') && (data[8] >= '0' && data[8] <= '9') && (data[9] >= '0' && data[9] <= '9')  ){
+        ano  = ((data[6] - '0')* 1000) + ((data[7] - '0') * 100) + ((data[8] - '0')*10) + (data[9]-'0');
+    }
+
+    if (dia == 0){
+        throw invalid_argument("Dia 0 nao existe\n");
+    }
+
+    if(mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12){
+        if(dia > 31){
+            throw invalid_argument("Mes com no maximo 31 dias\n");
+        }
+    }
+    else if(mes == 4 || mes == 6 || mes == 9 || mes == 11){
+        if (dia > 30){
+            throw invalid_argument("Mes com no maximo 30 dias\n");
+        }
+    }
+    else if(mes == 2){
+        if(ano % 4 == 0 && (ano % 400 == 0 || ano % 100 != 0)){
+            bissexto = true;
+        }
+        if(bissexto){
+            if(dia > 29){
+                throw invalid_argument("Dia inexistente\n");
+            }
+        }
+        else{
+            if(dia > 28){
+                throw invalid_argument("Dia inexistente\n");
+            }
+        }
+    }
+    else{
+        throw invalid_argument ("Mes inexistente\n");
+    }
+
+    if(ano < 1900 || ano > 2099)
+        throw invalid_argument("Ano fora dos parametros\n");
+
+}
+//-----------------------------------------------------------
+
+//Metodos de classe para o correio eletronico
+void Email::validar(string email)throw (invalid_argument){
+        int indice2 = email.find('@',0);
+        if(indice2 == -1)
+            throw  invalid_argument("Deve conter arroba");
+        //cout << indice2;
+        bool letra = false;
+//nao pode conter pontos no comeco ou no final
+
+        if(email[0] == '.' || email[indice2 - 1] == '.')
+
+            throw  invalid_argument("Nao pode existir espaco no comeco ou no final da parte local");
+
+//nao pode conter hifen no comeco nem final do dominio
+
+        if(email[indice2+1] == '-' || email[email.length()-1] == '-')
+
+            throw  invalid_argument("Nao pode conter hifens nesses lugares");
+
+
+//Nao pode conter só numeros
+        for(int indice = indice2 + 1; indice < email.length() ;indice++){
+            if(isalpha(email[indice]))
+                letra = true;
+            if(!isalpha(email[indice]) && !isdigit(email[indice]) && email[indice]!='.')
+                throw  invalid_argument("O dominio nao pode conter caracteres especiais");
+}
+        if (letra == false)
+
+            throw  invalid_argument("o dominio nao pode ser so de numeros");
 }
 //-------------------------------------------------------------------------
 
-
 //Métodos da Classe Senha
-void Senha::setSenha(string senha)throw (invalid_argument){
-    this->senha = senha;
-}
-
-string Senha::getSenha(){
-    return this->senha;
-}
 void Senha::validar(string senha)throw (invalid_argument){
-if(senha.lenth() != 8)
-    throw "A senha deve conter exatamente 8 caracteres"
-for(int indice = 0; indice < senha.length() - 1; indice++){
     bool maiuscula = false,minuscula = false,numero = false;
+    if(senha.length() != 8)
+    throw invalid_argument ("A senha deve conter exatamente 8 caracteres");
 
-    if (!isalpha(senha[indice]) && !isdigit(senha[indice]))
-        throw "A senha nao pode conter caracteres especiais";
+    for(int indice = 0; indice < senha.length() - 1; indice++){
 
-    if(islower(senha[indice]))
-        minuscula = true;
-    else if(isupper(senha[indice]))
-        maiuscula = true;
-    else if(isdigit(senha[indice]))
-        numero = true;
+        if (!isalpha(senha[indice]) && !isdigit(senha[indice]))
+            throw invalid_argument ("A senha nao pode conter caracteres especiais");
+
+        if(islower(senha[indice]))
+            minuscula = true;
+        else if(isupper(senha[indice]))
+            maiuscula = true;
+        else if(isdigit(senha[indice]))
+            numero = true;
+    }
+        if(!numero || !minuscula || !maiuscula)
+            throw invalid_argument ("A senha deve contar pelo menos uma letra maiúscula,uma minúscula e um digito");
+
 }
-    if(!numero || !minuscula || !maiuscula)
-        throw "A senha deve contar pelo menos uma letra maiúscula,uma minúscula e um digito";
-
-
     //if(nome.find(senha) != std::npos)
        // throw "A senha nao pode conter o nome do usuario"
 //------------------------------------------------------------------------
 
-
 //Métodos da Classe Texto
-void Texto::setTexto(string texto)throw (invalid_argument){
-    this->texto = texto;
-}
-
-string Texto::getTexto(){
-    return this->texto;
-}
-
 void Texto::validar(string texto)throw (invalid_argument){
-    if(texto.length() > 30)
-        throw "Texto maior que o limite permitido\n";
+    if(texto.length() > 30 || texto.length() == 0)
+        throw invalid_argument ("Texto maior que o limite permitido\n");
 }
 //------------------------------------------------------------------------
 
+//Métodos da Classe Idioma
+void Idioma::validar(string idioma) throw(invalid_argument){
+    if(idioma != "ENG" && idioma != "FRA" && idioma != "GER" && idioma != "ITA" && idioma != "POR" && idioma != "SPA"){
+        throw invalid_argument("Idioma selecionado nao existe\n");
+    }
+}
+//------------------------------------------------------------------------
 
-
-
-
+//Métodos da Classe Termos
+void Termo::validar(string termo)throw(invalid_argument){
+    if(termo != "PT" && termo != "NP"){
+        throw invalid_argument("Termo selecionado nao existe\n");
+    }
+}
+//----------------------------------------------------------------------
