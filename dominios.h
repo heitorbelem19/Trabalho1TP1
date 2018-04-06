6a65867d286f7c35db1a8f794e50060ec8b65c9a
heitorbelem19 <32557513+heitorbelem19@users.noreply.github.com>
2018-04-06 17:50:38 -0300

    Update dominios.h

--- dominios.h
+++ dominios.h
@@ -1,89 +1,158 @@
 #ifndef _DOMINIO_H_INCLUDED
 #define _DOMINIO_H_INCLUDED
 #include <bits/stdc++.h>
-
 using namespace std;
 
 class Nome{
 private:
-    string nome;
+    string name;
     void validar(string nome)throw (invalid_argument);
 public:
-    void setNome(string nome)throw (invalid_argument);
-    string getNome();
+    Nome();
+    Nome(string nome){setNome(nome);}
+    ~Nome(){};
+    inline void setNome(string nome)throw (invalid_argument){
+        validar(nome);
+        this->name = nome;
+    }
+    inline string getNome(){return name;}
 };
 
 class Sobrenome{
 private:
-	string sobrenome;
+	string last_name;
 	void validar(string sobrenome)throw (invalid_argument);
 public:
-	void setSobrenome(string sobrenome)throw (invalid_argument);
-	string getSobrenome();
+    Sobrenome();
+    Sobrenome(string sobrenome){setSobrenome(sobrenome);}
+    ~Sobrenome(){};
+	inline void setSobrenome(string sobrenome)throw (invalid_argument){
+        validar(sobrenome);
+        this->last_name = sobrenome;
+    }
+	inline string getSobrenome(){return last_name;}
 };
 
 class Telefone{
 private:
-	string telefone;
+	string telephone;
 	void validar(string telefone)throw (invalid_argument);
 public:
-	void setTelefone(string telefone)throw (invalid_argument);
-	string getTelefone();
+    Telefone();
+    Telefone(string telefone){setTelefone(telefone);}
+    ~Telefone(){};
+	inline void setTelefone(string telefone)throw (invalid_argument){
+        validar(telefone);
+        this->telephone = telefone;
+	}
+	inline string getTelefone(){return telephone;}
 };
 
 class Endereco{
 private:
-	string endereco;
+	string address;
 	void validar(string endereco)throw (invalid_argument);
 public:
-	void setEndereco(string endereco)throw (invalid_argument);
-	string getEndereco();
+    Endereco();
+    Endereco(string endereco){setEndereco(endereco);}
+    ~Endereco(){};
+	inline void setEndereco(string endereco)throw (invalid_argument){
+        validar(endereco);
+        this->address = endereco;
+	}
+	inline string getEndereco(){return address;}
 };
 
 class Data{
 private:
-	string data;
+	string date;
 	void validar(string data)throw (invalid_argument);
 public:
-	void setData(string data)throw (invalid_argument);
-	string getData();
+    Data();
+    Data(string data){setData(data);}
+    ~Data(){};
+	inline void setData(string data)throw(invalid_argument){
+        validar(data);
+        this->date = data;
+	}
+	inline string getData(){return date;}
 };
 
 class Email{
 private:
-    string email;
+    string email_address;
     void validar(string email) throw(invalid_argument);
 public:
-    void setEmail(string email)throw(invalid_argument);
-    string getEmail();
-
+    Email();
+    Email(string email){setEmail(email);}
+    ~Email(){};
+    inline void setEmail(string email)throw(invalid_argument){
+        validar(email);
+        this->email_address = email;
+    }
+    inline string getEmail(){return email_address;}
 };
 
 class Senha{
 private:
-    string senha;
+    string password;
     void validar(string senha) throw (invalid_argument);
 public:
-    void setSenha(string senha)throw (invalid_argument)
-    string getSenha();
+    Senha();
+    Senha(string senha){setSenha(senha);}
+    ~Senha(){};
+    inline void setSenha(string senha)throw (invalid_argument){
+        validar(senha);
+        this->password = senha;
+    }
+    inline string getSenha(){return password;}
 };
 
 class Texto{
 private:
-	string texto;
+	string text;
 	void validar(string texto)throw (invalid_argument);
 public:
-	void setTexto(string texto)throw (invalid_argument);
-	string getTexto();
+    Texto();
+    Texto(string texto){setTexto(texto);}
+    ~Texto(){};
+	inline void setTexto(string texto)throw (invalid_argument){
+        validar(texto);
+        this->text = texto;
+	}
+	inline string getTexto(){return text;}
 };
+
 
 class Idioma{
 private:
-	string idioma;
-	void validar()throw (invalid_argument);
+	string language;
+	void validar(string idioma)throw (invalid_argument);
 public:
-	void setIdioma(string idioma)throw (invalid_argument);
-	string getIdioma();
+    Idioma();
+    Idioma(string idioma){setIdioma(idioma);}
+    ~Idioma(){};
+	inline void setIdioma(string idioma)throw (invalid_argument){
+        validar(idioma);
+        this->language = idioma;
+	}
+	inline string getIdioma(){return language;}
+};
+
+
+class Termo{
+private:
+    string term;
+    void validar(string termo) throw (invalid_argument);
+public:
+    Termo();
+    Termo(string termo){setTermo(termo);}
+    ~Termo(){};
+    inline void setTermo(string termo)throw (invalid_argument){
+        validar(termo);
+        this->term = termo;
+    }
+    inline string getTermo(){return term;}
 };
 
 #endif // _DOMINIO_H_INCLUDED
