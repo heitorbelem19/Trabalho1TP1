585add4bdf5482541d97133c2108313948b60396
heitorbelem19 <32557513+heitorbelem19@users.noreply.github.com>
2018-04-13 13:29:13 -0300

    Update teste_dominio.h

--- teste_dominio.h
+++ teste_dominio.h
@@ -1,16 +1,43 @@
+#ifndef TESTES_H_INCLUDED
+#define TESTES_H_INCLUDED
 #include "dominios.h"
 using namespace std;
 
 class TUNome{
 private:
+    //const static int VALOR_VALIDO   = 20;
+    //const static int VALOR_INVALIDO = 30;
+    Nome *nome;
+    int estado;
     void setUp();
     void tearDown();
-    void testarSucesso();
-    void testarFalha();
-    int estado;
-    Nome *nome;
+    void testarCenarioSucesso();
+    void testarCenarioFalha();
+
 public:
-    int run();
-    const static int Sucesso = 0;
-    const static int Falha = -1;
+    const static int SUCESSO =  0;
+    const static int FALHA   = -1;
+    void runNome();
 };
+
+class TUSobrenome{
+private:
+    //const static int VALOR_VALIDO   = 20;
+    //const static int VALOR_INVALIDO = 30;
+    Sobrenome *sobrenome;
+    int estado;
+    void setUp();
+    void tearDown();
+    void testarCenarioSucesso();
+    void testarCenarioFalha();
+
+public:
+    const static int SUCESSO =  0;
+    const static int FALHA   = -1;
+    void runSobrenome();
+};
+
+
+#endif // TESTES_H_INCLUDED
+
+
