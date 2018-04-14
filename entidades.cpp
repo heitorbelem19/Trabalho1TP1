70d0a30d6bdc19730dc1c8785faf3b7ad5630fbc
heitorbelem19 <32557513+heitorbelem19@users.noreply.github.com>
2018-04-14 12:22:35 -0300

    Create entidades.cpp

---
+++ entidades.cpp
@@ -0,0 +1,10 @@
+#include "entidades.h"
+
+Leitor::Leitor(string nome,string sobrenome,string email, string senha){
+    nome.setNome(string nome);
+    sobrenome.setSobrenome(string sobrenome);
+    senha.setSenha(string senha);
+    email.setEmail(string email);
+}
+
+//NÃO TA FUNCIONANDO
