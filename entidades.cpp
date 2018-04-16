0aab6d0b4a0660566acdcb23f9f6b2808e42746d
heitorbelem19 <32557513+heitorbelem19@users.noreply.github.com>
2018-04-16 19:44:18 -0300

    Update entidades.cpp

--- entidades.cpp
+++ entidades.cpp
@@ -11,12 +11,33 @@ Leitor::Leitor(){
     this->senha = Senha();
     this->email = Email();
 }
+
+bool Leitor::CheckSubstring(string firstString,string secondString){
+    for (unsigned int i = 0; i < firstString.size(); i++){
+        int j = 0;
+        if(firstString[i] == secondString[j]){
+            while (firstString[i] == secondString[j] && j < secondString.size()){
+                j++;
+                i++;
+            }
+
+            if (j == secondString.size())
+                    return true;
+        }
+    }
+    return false;
+}/**
+ * Funcao destinada a verificacao da existencia da string nome, na string senha
+ * @return Retorna true se o nome estiver contido na senha e false se nao estiver.
+ */
+
+
 /**
  * Funcao destinada a criacao do Leitor indicado pelos campos informados pelos argumentos
  * @return Retorna se foi possivel ou nao criar a entidade leitor.
  */
 bool Leitor::Criar_leitor(string nome,string sobrenome,string email, string senha){
-    if(!this->nome.setNome(nome) || !this->sobrenome.setSobrenome(sobrenome) || !this->senha.setSenha(senha) || !this->email.setEmail(email))
+    if(!this->nome.setNome(nome) || !this->sobrenome.setSobrenome(sobrenome) || !this->senha.setSenha(senha) || CheckSubstring(this->senha.getSenha(),this->nome.getNome()) || !this->email.setEmail(email))
         return false;
     return true;
 }
